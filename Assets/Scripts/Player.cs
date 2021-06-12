8f97ca840c68b5f7cc545794b09b56ab9eca8e98
Matteo Lo Piccolo <lostsoul81x@gmail.com>
2021-06-12 19:34:05 +0200

    Add Cooldown System

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,12 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 3.5f;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.5f;
+
+    private float _canFire = -1;
 
 
     // Start is called before the first frame update
@@ -18,6 +24,17 @@ public class Player : MonoBehaviour
     void Update()
     {
         CalculateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            FireLaser();
+        }
+    }
+
+    public void FireLaser()
+    {
+        _canFire = Time.time + _fireRate;
+        Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
     }
 
     private void CalculateMovement()
