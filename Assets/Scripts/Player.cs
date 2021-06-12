f954ae9bf64f65a2c91c6f6d52ed9e8ea246fdd7
Matteo Lo Piccolo <lostsoul81x@gmail.com>
2021-06-12 21:37:28 +0200

    Import assets

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -41,7 +41,7 @@ public class Player : MonoBehaviour
     public void FireLaser()
     {
         _canFire = Time.time + _fireRate;
-        Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
+        Instantiate(_laserPrefab, transform.position + new Vector3(0, 1.05f, 0), Quaternion.identity);
     }
 
     private void CalculateMovement()
